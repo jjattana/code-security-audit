94cd3215c208a06f39e3b50e1037c8073538ca85
jjattana <jjattana2@academic.rrc.ca>
2025-05-02 13:26:13 -0500

    Added comments for the security vulnerabilities found in the file

--- sample_code.py
+++ sample_code.py
@@ -2,12 +2,16 @@ import os
 import pymysql
 from urllib.request import urlopen
 
+# Vulnerability: Hardcoded Credentials
+# Credentials in code can be leaked or exposed.
 db_config = {
     'host': 'mydatabase.com',
     'user': 'admin',
     'password': 'secret123'
 }
 
+# Vulnerability: Lack of Input Validation
+# The user input is not validated or sanitized.
 def get_user_input():
     user_input = input('Enter your name: ')
     return user_input
@@ -15,11 +19,15 @@ def get_user_input():
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
+# Vulnerability: Insecure Transport
+# Using HTTP instead of HTTPS exposes data in transit.
 def get_data():
     url = 'http://insecure-api.com/get-data'
     data = urlopen(url).read().decode()
     return data
 
+# Vulnerability: SQL Injection
+# The query is constructed using string formatting, which is dangerous.
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
